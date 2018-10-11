8956baa8d6646f51ffbb057cad481d9aff9e6ee6
Kwame <kwame.mintah@cgi.com>
2018-10-11 14:07:54 +0000

    Adding def for oystercard and tests for rspec

---
+++ lib/oystercard.rb
@@ -0,0 +1,31 @@
+class Oystercard
+  MAXIMUM_BALANCE = 90
+  attr_reader :balance, :MAXIMUM_BALANCE, :in_journey
+  
+  def initialize
+    @balance = 0
+    @in_journey = false
+  end
+  
+  def top_up(amount)
+    fail 'Maximum balance exceeded' if amount + balance > MAXIMUM_BALANCE
+    @balance += amount
+  end
+  
+  def deduct(amount)
+    @balance -= amount
+  end
+
+  def in_journey?
+    @in_journey
+  end
+
+  def touch_in
+    fail "Insufficient balance to touch in" if balance < 1
+    @in_journey = true
+  end
+  
+  def touch_out
+    @in_journey = false
+  end
+end
